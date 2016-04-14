2082b3fe1fed5d41fbf46c0b30c057bdba251a54
Steve Kondik <steve@cyngn.com>
2016-04-14 02:52:21 -0700

    mm: Backport ZCache from QC kernel 3.18
    [fcuzzocrea] :
    
     * also edit
    
        fs/debugfs/file.c
        include/linux/debugfs.h
    
       to add missing functions required from newer zcache not present in the
       nvidia kernel and edit :
    
        lib/fault-inject.c
    
       accordingly.
    
     * also edit pageflag_names[] in mm/page_alloc.c struct to add PG_was_active, to reflect the
       change added in mm/page_alloc.c to avoid triggering BUILD_BUG_ON due to
       the mismatch between the size of (pageflag_names - 1) and __NR_PAGEFLAGS
    
    Change-Id: I3edff3a56cf6525f13430ab93309272d1faecfe1
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6289,6 +6289,9 @@ static const struct trace_print_flags pageflag_names[] = {
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	{1UL << PG_compound_lock,	"compound_lock"	},
 #endif
+#ifdef CONFIG_ZCACHE
+	{1UL << PG_was_active,		"was_active"	},
+#endif
 };
 
 static void dump_page_flags(unsigned long flags)
