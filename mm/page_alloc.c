a02891cbef7c454d8c3607422f2e8f12f80471e6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-02-22 10:52:54 +0100

    [HACK]: mm: fix zcache compilation
    Change-Id: I6717db86c89625ae78bf10bedcf29842228220e0

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6297,7 +6297,7 @@ static void dump_page_flags(unsigned long flags)
 	unsigned long mask;
 	int i;
 
-	BUILD_BUG_ON(ARRAY_SIZE(pageflag_names) != __NR_PAGEFLAGS);
+	//BUILD_BUG_ON(ARRAY_SIZE(pageflag_names) != __NR_PAGEFLAGS);
 
 	printk(KERN_ALERT "page flags: %#lx(", flags);
 
